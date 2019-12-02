ca4ef9668d3e2ea9b70dae6648d7618170011a40
Ingride Ngaku <ingride.i.ngaku@gmail.com>
2019-12-01 20:57:05 -0500

    Created activities and uploaded background screenshots

---
+++ app/src/main/java/com/cmsmc434/latitudepals/YourLocation.java
@@ -0,0 +1,14 @@
+package com.cmsmc434.latitudepals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YourLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
