c851518738c80b03dfb30beb274b20b0603b25b2
Logan Harris <harris.logan@outlook.com>
2019-12-06 14:29:18 -0500

    Further progress on various buttons and addition of feed reaction feature. Re-uploaded appropriate background images for reactions

--- app/src/main/java/com/cmsmc434/latitudepals/ReactJen.java
+++ app/src/main/java/com/cmsmc434/latitudepals/ReactJen.java
@@ -2,7 +2,10 @@ package com.cmsmc434.latitudepals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class ReactJen extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class ReactJen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reactjen);
+    }
+
+    //goes to home screen
+    public void goToHomescreen(View view){
+        Intent intent= new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    //goes back to feed and "sends the reaction"
+    public void sendReaction(View view){
+        Intent intent= new Intent(this, Feed.class);
+        Toast toast = Toast. makeText(getApplicationContext(),"Reaction has been sent!",Toast. LENGTH_SHORT);
+        toast. setMargin(50,50);
+        toast. show();
+        startActivity(intent);
     }
 }
